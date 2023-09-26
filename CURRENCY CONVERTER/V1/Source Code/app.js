e30edfa76f513541d5a6c15a56cf4b0e00f4cca7
PATEL KRISH <127637917+PATEL-KRISH-0@users.noreply.github.com>
2023-09-26 23:18:56 +0530

    Add files via upload

---
+++ CURRENCY CONVERTER/V1/Source Code/app.js
@@ -0,0 +1,8 @@
+const output = document.querySelector(".output");
+const submit = document.querySelector(".Submit");
+
+submit.addEventListener("click", () => {
+    const money = document.getElementById("money").value;
+    output.textContent = `the indian rs ${money} is in us doller = ${money * 83.23}`
+})
+
